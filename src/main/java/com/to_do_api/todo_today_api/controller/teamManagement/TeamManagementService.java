a28ae31534e0ec81f783b77cd0a26ad964b8d07c
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2024-08-24 13:06:39 +0200

    (Implemented recurses that allows to update te database asociating a user with a group with the recurse /associateUser)

--- src/main/java/com/to_do_api/todo_today_api/controller/teamManagement/TeamManagementService.java
+++ src/main/java/com/to_do_api/todo_today_api/controller/teamManagement/TeamManagementService.java
@@ -7,6 +7,8 @@ import com.to_do_api.todo_today_api.repo.teams.Client_team_association;
 import com.to_do_api.todo_today_api.repo.teams.RepositoryClientTeamAssociation;
 import com.to_do_api.todo_today_api.repo.teams.RepositoryTeams;
 import com.to_do_api.todo_today_api.repo.teams.Teams;
+import com.to_do_api.todo_today_api.repo.user.RepositoryUser;
+import com.to_do_api.todo_today_api.repo.user.User;
 import com.to_do_api.todo_today_api.repo.user_temporal_tokens.RepositoryTemporalTokens;
 import com.to_do_api.todo_today_api.repo.user_temporal_tokens.User_temporal_token;
 
@@ -14,12 +16,16 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 
 @RestController
 @RequestMapping("/teams")
 public class TeamManagementService {
+    @Autowired
+    private RepositoryUser repositoryUser;
+
     @Autowired
     private RepositoryTeams repositoryTeams;
     
@@ -54,8 +60,33 @@ public class TeamManagementService {
 
             jsonTeams.put(teams.getTeamkey(), team);
         }
-
-        System.out.println(jsonTeams.toString());
         return ResponseEntity.ok(jsonTeams.toString());
+    }
+
+    @PostMapping("/associateUser")
+    public ResponseEntity<String> associateUser(@RequestHeader("Authorization") String tkn, @RequestBody String body) {
+        String token = tkn.replace("Bearer ", "");
+        
+        User_temporal_token user_temporal_token = repositoryTemporalTokens.findByToken(token);
+        if (user_temporal_token != null) {
+            User user = repositoryUser.findById(user_temporal_token.getUserId());
+
+            Client_team_association userExist = repositoryClientTeamAssociation.findByUserId(user.getId());
+            if (userExist != null) {
+                return ResponseEntity.ok(new JSONObject().put("association", false).toString());
+            }
+            
+            JSONObject teamJson = new JSONObject(body);
+
+            repositoryClientTeamAssociation.save(new Client_team_association(user.getId(), teamJson.getString("groupId")));
+            
+            // ? Set user value isInGroup to 1
+            user.setInGroup(true);
+            repositoryUser.save(user);
+            
+            return ResponseEntity.ok(new JSONObject().put("association", true).toString());
+        }
+        
+        return ResponseEntity.ok(new JSONObject().put("association", false).toString());
     }
 }
